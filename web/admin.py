f85ddab2baf9187100aa1aff09d2ece8eb543bf7
hamid azimi <hamid.h.a44100@gmail.com>
2020-08-28 21:28:23 +0430

    expense model is there

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+# Register your models here
+
+from .models import Expense
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    pass
--- web/models.py
+++ web/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+class Expense(models.Model):
+    text = models.CharField(max_length = 300)
+    date = models.DateTimeField()
+    amount = models.BigIntegerField()
+    user = models.ForeignKey(User, on_delete = models.SET_NULL, null = True, blank = True)
+    
+    
+    class Meta:
+        ordering = ["date", "amount"]
+        
+    
+    def __str__(self):
+        return "{0}  <{1}>".format(self.text, self.amount)
