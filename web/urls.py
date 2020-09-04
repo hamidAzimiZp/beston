0a4a032fb76272059be8a434def469609267923f
hamid azimi <hamid.h.a44100@gmail.com>
2020-09-04 19:17:04 +0430

    cheange versions of jquery bootstrap

--- web/urls.py
+++ web/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
 
 from . import views
+from django.views.generic import RedirectView
 
 
 
@@ -13,5 +14,5 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path("account/dashboard/", views.dashboard, name = "dashboard"),
     path("account/dashboard/status", views.user_status, name = "userStatus"),
-    
+    path("favicon\.ico/", RedirectView.as_view(url='/static/_images/favicon.ico'))
 ]
