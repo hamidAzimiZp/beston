464606af721675f890c30daac6628c8dc9f1e746
hamid azimi <hamid.h.a44100@gmail.com>
2020-09-05 15:05:30 +0430

    modify css styles

--- media/static/_js/custom.js
+++ media/static/_js/custom.js
@@ -2,3 +2,13 @@
 $(".close").on("click", function(){
     $(this).parent().fadeOut(1000)
 })
+
+// open and close navigation menu for small devices
+
+$(".navButton").on("click", ()=>{
+    $(".my_nav2").toggleClass("show_nav")
+})
+// close when click on body
+$(".wrapper").on("click", function(){
+    $(".my_nav2").removeClass("show_nav")
+})
